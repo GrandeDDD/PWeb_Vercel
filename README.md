4a03356e29032f89ef757e58d9dfa588a6352ce0
GrandeDDD <diego5.da21@gmail.com>
2023-12-02 17:03:18 -0300

    Deletando teste

--- README.md
+++
@@ -1,2 +0,0 @@
-# P-Web
-Repositório criado com o intuito de fazer o deploy das receitas da Unidade 3 por meio da plataforma Vercel
--- pages/santana.js
+++
@@ -1,3 +0,0 @@
-export function Exportsantana() {
-    return <h2>A festa de Sant'Ana é comemorada todos os anos da cidade de Caicó</h2>
-}
