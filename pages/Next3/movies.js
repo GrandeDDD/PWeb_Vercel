ea8751ea79130ea44b352c6b0f3e5ad8ded1f22f
GrandeDDD <diego5.da21@gmail.com>
2023-12-03 14:55:19 -0300

    Teste

--- pages/Next3/movies.js
+++ pages/Next3/movies.js
@@ -1,54 +1,45 @@
-import { useState } from "react";
-
-function Movie({ movie }) {
-  return (
-    <div key={movie.imdbID}>
-      <h2>
-        {movie.Title} ({movie.Year}) --- {movie.Type}
-      </h2>
-      <img src={movie.Poster} width="180px" alt={movie.Title} />
-    </div>
-  );
-}
-
-function MoviesList({ movies }) {
-  return (
-    <div>
-      {movies.map((movie) => (
-        <Movie key={movie.imdbID} movie={movie} />
-      ))}
-    </div>
-  );
-}
+import { useState, useEffect } from "react";
 
 export default function Movies({ initialData }) {
-  const [searchTerm, setSearchTerm] = useState("bagdad");
-  const [data, setData] = useState(initialData);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [data, setData] = useState(initialData);
 
-  const fetchData = async () => {
-    const res = await fetch(
-      `http://www.omdbapi.com/?apikey=ee5ef93d&s=${searchTerm}`
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch(`https://www.omdbapi.com/?apikey=ca98445&s=${searchTerm}`);
+            const searchData = await res.json();
+            setData(searchData);
+        }
+
+        if (searchTerm) {
+            fetchData();
+        }
+    }, [searchTerm]);
+
+    function handleSearch(e) {
+        setSearchTerm(e.target.value);
+    }
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Digite a palavra-chave"
+                    value={searchTerm}
+                    onChange={handleSearch}
+                />
+            </div>
+            <div >
+                {data && data.Search && data.Search.map((movie) => (
+                    <div key={movie.imdbID}>
+                        <img src={movie.Poster} width="180px" alt={movie.Title} />
+                        <h2>
+                            {movie.Title} --- {movie.Year} --- {movie.Type}
+                        </h2>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
-    const searchData = await res.json();
-    setData(searchData);
-  };
-
-  // Chamada inicial
-  fetchData();
-
-  return (
-    <div style={{ textAlign: "center" }}>
-      <div>
-        <input
-          type="text"
-          placeholder="Digite a palavra-chave"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
-      <div>
-        {data && data.Search && <MoviesList movies={data.Search} />}
-      </div>
-    </div>
-  );
 }
