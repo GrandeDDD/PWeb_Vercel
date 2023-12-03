a8455f5aa6ecae817315847eaa0588dcae5bb543
GrandeDDD <diego5.da21@gmail.com>
2023-12-02 22:22:35 -0300

    Next2

---
+++ pages/Next2/outroDir.js
@@ -0,0 +1,10 @@
+import  Exportsantana  from "../santana"
+
+export default function OutroDir() {
+  return (
+    <div>
+      <h2>Puxando informações de outro diretório</h2>
+      <Exportsantana/>
+    </div>
+  )
+}
